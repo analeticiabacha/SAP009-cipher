1bea02e264b4cc3880a759c22c6a70956dc63d52
Ana <ana_leticia@geb.inatel.br>
2023-01-16 17:10:26 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+const botaoResultado = document.getElementById("BotaoResultado");
+const opcaoCodificar = document.getElementById("OpcaoCodificar");
+const opcaoDecodificar = document.getElementById("OpcaoDecodificar");
+const textoCodifica = document.getElementById("CodificaArea");
+
+botaoResultado.addEventListener("click" , acaoDoBotao);
+
+function acaoDoBotao(){
+    //console.log("função funcionando");
+    if (opcaoCodificar.checked) {
+        console.log("Opção CODIFICAR selecionada");
+
+    } else {
+        console.log("Opção DECODIFICAR selecionada");
+    }
+}

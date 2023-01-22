4f4f487b3e46a1d6d0fd2a473e11dd050b32f86f
Ana <ana_leticia@geb.inatel.br>
2023-01-22 18:55:33 -0300

    finalizando ajustes finais

--- src/cipher.js
+++ src/cipher.js
@@ -1,54 +1,38 @@
 const cipher = {
-  
   encode: function (deslocamento, textoCodifica){    
-    console.log(textoCodifica.value);
     let resultado = "";
-    
     let varTeste = textoCodifica.value;
-    let offset = Number(deslocamento.value);
-    
+    const offset = Number(deslocamento.value);
     varTeste = varTeste.toUpperCase();
 
     for (let i = 0; i < varTeste.length; i++ ){
-      
-      let posicao = varTeste.charCodeAt(i);
-
+      const posicao = varTeste.charCodeAt(i);
       if(posicao >= 65 && posicao <= 90){
-        
-        let resultadoCod = (posicao - 65 + offset) % 26 + 65;
-        
-        resultado = resultado + String.fromCharCode(resultadoCod); 
-        console.log( posicao , "+" , offset , "=" , resultadoCod , "=>" , String.fromCharCode(resultadoCod) );
-
+        const resultadoCod = (posicao - 65 + offset) % 26 + 65;
+        resultado += String.fromCharCode(resultadoCod); 
+        //console.log( posicao , "+" , offset , "=" , resultadoCod , "=>" , String.fromCharCode(resultadoCod) );
       }
-      else if ((posicao >= 32 && posicao <= 64) || (posicao >= 91 && posicao <= 126)) { //caracteres e espaço da tabela ASC
+      else if ((posicao >= 32 && posicao <= 64) || (posicao >= 91 && posicao <= 126)) { //caracteres especiais e espaço da tabela ASC
         resultado += varTeste.charAt(i);  
-      }
-      
+      } 
     }
     return resultado;
   },
 
   decode: function (deslocamento, textoCodifica){
-    console.log(textoCodifica.value);
+    
     let resultado2 = "";
-    
     let varTeste = textoCodifica.value;
-    let offset = Number(deslocamento.value);
-    
+    const offset = Number(deslocamento.value);
     varTeste = varTeste.toUpperCase();
 
     for (let i = 0; i < varTeste.length; i++ ){
       
-      let posicao = varTeste.charCodeAt(i);
-
+      const posicao = varTeste.charCodeAt(i);
       if(posicao >= 65 && posicao <= 90){
-        
-        let resultadoCod = (posicao + 65 - offset) % 26 + 65;
-        
+        const resultadoCod = (posicao + 65 - offset) % 26 + 65;
         resultado2 = resultado2 + String.fromCharCode(resultadoCod); 
-        console.log( posicao , "+" , offset , "=" , resultadoCod , "=>" , String.fromCharCode(resultadoCod) );
-
+        //console.log( posicao , "+" , offset , "=" , resultadoCod , "=>" , String.fromCharCode(resultadoCod) );
       }
       else if ((posicao >= 32 && posicao <= 64) || (posicao >= 91 && posicao <= 126)) { //caracteres e espaço da tabela ASC
         resultado2 += varTeste.charAt(i);  
@@ -57,6 +41,7 @@ const cipher = {
     return resultado2;
   }
 }
+
 export default cipher;
 
 

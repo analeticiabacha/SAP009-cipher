d73741ef0c59369d214237b75c04545fecb9413d
Ana <ana_leticia@geb.inatel.br>
2023-01-22 19:45:06 -0300

    alteracoes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
 
 const botaoResultado = document.getElementById("BotaoResultado");
 const opcaoCodificar = document.getElementById("OpcaoCodificar");
@@ -12,18 +12,17 @@ const deslocamento = document.getElementById("ChaveSecreta");
 botaoResultado.addEventListener("click",acaoDoBotao);
 
 function acaoDoBotao(){
-    //console.log(textoCodifica.value);
-    if (textoCodifica.value == "" || deslocamento == "" || opcaoCodificar == "" || opcaoDecodificar == ""){
-        alert("Por favor, preencha todos os campos!");  
+  if (textoCodifica.value === "" || deslocamento === "" || opcaoCodificar === "" || opcaoDecodificar === ""){
+    alert("Por favor, preencha todos os campos!");  
     
-    }
-    else if (opcaoCodificar.checked) {
-        textoResultado.value = cipher.encode(deslocamento, textoCodifica); 
-        console.log(textoResultado.value);      
+  }
+  else if (opcaoCodificar.checked) {
+    textoResultado.value = cipher.encode(deslocamento, textoCodifica); 
+    console.log(textoResultado.value);      
 
-    } else {
-        //console.log("Opção DECODIFICAR selecionada");
-        textoResultado.value = cipher.decode(deslocamento, textoCodifica); 
-        console.log(textoResultado.value);
-    } 
+  } else {
+    
+    textoResultado.value = cipher.decode(deslocamento, textoCodifica); 
+    console.log(textoResultado.value);
+  } 
 }
